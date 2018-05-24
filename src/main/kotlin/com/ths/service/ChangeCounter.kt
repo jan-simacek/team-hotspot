de92081e4137ca77649174dc442222e8567e9cc2
Jan Simacek <jan.simacek@infor.com>
2018-05-24 13:19:31 +0200

    parallelization but i don't think it works

--- src/main/kotlin/com/ths/service/ChangeCounter.kt
+++ src/main/kotlin/com/ths/service/ChangeCounter.kt
@@ -3,19 +3,34 @@ package com.ths.service
 import com.google.common.collect.Multimaps
 import com.ths.model.RevisionAndBranch
 import com.ths.svn.SvnClient
+import kotlinx.coroutines.experimental.Deferred
+import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.runBlocking
 import org.apache.log4j.Logger
+import java.util.concurrent.atomic.AtomicInteger
 
 private val log = Logger.getLogger(BranchAndNoOfChanges::class.java)
 
-data class BranchAndNoOfChanges(val branchUrl: String, val noOfChanges: Int)
+data class BranchAndNoOfChanges(val branchUrl: String, val author: String, val noOfChanges: Int)
+/**
+ * NOT THREAD SAFE!
+ */
 class ChangeCounter(private val svnClient: SvnClient) {
-    private fun processDiff(path: String, revisions: Pair<Long, Long>): Int {
+    private var noOfDiffsLoaded = AtomicInteger(0)
+    private var noOfDiffsToLoad = 0
+
+    data class DeferedBranchAndChanges(val branchUrl: String, val author: String, val noOfChanges: Deferred<Int>) {
+        suspend fun await(): BranchAndNoOfChanges {
+            return BranchAndNoOfChanges(branchUrl, author, noOfChanges.await())
+        }
+    }
+    private fun processDiff(path: String, revisions: Pair<Long, Long>): Deferred<Int> = async {
         val diff = svnClient.loadDiff(path, revisions)
-        return DiffInterpreter.retrieveNumLinesChanged(diff)
+        log.info("Loaded diff ${noOfDiffsLoaded.incrementAndGet()} of $noOfDiffsToLoad")
+        DiffInterpreter.retrieveNumLinesChanged(diff)
     }
 
-    private fun countChangesForPath(changedPath: ChangedPath, branches: Collection<RevisionAndBranch>): List<BranchAndNoOfChanges> {
-        log.debug("Processing log for $changedPath")
+    private fun countChangesForPath(changedPath: ChangedPath, branches: Collection<RevisionAndBranch>): List<DeferedBranchAndChanges> {
         val branchToRevisions = Multimaps.transformValues(Multimaps.index(branches, { it!!.branch }), { it!!.revision })
         val branchToRevisionRanges = branchToRevisions.asMap().entries.map {
             val revisions = it.value.map { it.number }
@@ -28,16 +43,20 @@ class ChangeCounter(private val svnClient: SvnClient) {
             Pair(it.key.baseUrl, Pair(min, max))
         }.toMap()
 
-        return branches.map { it.branch.baseUrl }.toSet().map {
-            BranchAndNoOfChanges(it, processDiff(it + changedPath, branchToRevisionRanges[it]!!))
+        return branches.map { Pair(it.branch.baseUrl, it.branch.author) }.toSet().map {
+            DeferedBranchAndChanges(it.first, it.second, processDiff(it.first + changedPath, branchToRevisionRanges[it.first]!!))
         }
     }
 
     fun countChanges(changedPathsAndRevisions: Map<ChangedPath, Collection<RevisionAndBranch>>): Map<ChangedPath, Collection<BranchAndNoOfChanges>> {
-        val noOfChangedPaths = changedPathsAndRevisions.size
-        return changedPathsAndRevisions.entries.mapIndexed { index, entry ->
-            log.info("Processing conflicting path ${index + 1} of $noOfChangedPaths")
-            Pair(entry.key, countChangesForPath(entry.key, entry.value))
-        }.toMap()
+        log.info("Found ${changedPathsAndRevisions.size} conflicts. Loading diffs.")
+        noOfDiffsToLoad = changedPathsAndRevisions.map { it.value.map { it.branch.baseUrl }.toSet().size }.sum()
+        return runBlocking {
+             changedPathsAndRevisions.entries.mapIndexed { index, entry ->
+                Pair(entry.key, countChangesForPath(entry.key, entry.value))
+            }.map {
+                Pair(it.first, it.second.map { it.await() })
+            }.toMap()
+        }
     }
 }
